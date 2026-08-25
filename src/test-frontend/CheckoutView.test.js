8fd20e918298e8ef9349554512fff70051f2aa85
ReggianiD <daviderreggiani@gmail.com>
2026-08-25 15:42:22 +0200

    style: formatta il file CheckoutView.test.js

--- src/test-frontend/CheckoutView.test.js
+++ src/test-frontend/CheckoutView.test.js
@@ -1,44 +1,87 @@
-import React from 'react';
-import { screen } from '@testing-library/react';
-import { renderWithProviders } from '../test-utils';
-import CheckoutView from '../react_redux/views/ordine_view/CheckoutView';
-
-describe('Debug Preliminare: CheckoutView', () => {
-  test('Rendering base e analisi HTML della vista checkout', () => {
-    const preloadedState = {
-      autenticazione: {
-        value: { 
-          id_utente: 1, 
-          isLogged: true, 
-          indirizzo: "Via Roma 10, Milano" 
-        },
-      },
-      stile: {
-        value: { vistaForm: 'form' },
-      },
-      carrello: {
-        value: { 
-          items: [
-            { 
-              id: 101, 
-              nome: "Servizio Test Checkout", 
-              prezzo: 25.00, 
-              quantita: 2, 
-              tipo: "Servizio" 
-            }
-          ] 
-        },
-      },
-      carta: {
-        value: { carte: [] },
-      },
-      servizio: {
-        value: { servizi: [] },
-      },
-    };
-
-    renderWithProviders(<CheckoutView />, { preloadedState });
-    
-    screen.debug();
-  });
-});
+import React from 'react';                                                                                                                                   
+                                                                                                                                                             
+import { screen } from '@testing-library/react';                                                                                                             
+                                                                                                                                                             
+import { renderWithProviders } from '../test-utils';                                                                                                         
+                                                                                                                                                             
+import CheckoutView from '../react_redux/views/ordine_view/CheckoutView';                                                                                    
+                                                                                                                                                             
+                                                                                                                                                             
+                                                                                                                                                             
+describe('Debug Preliminare: CheckoutView', () => {                                                                                                          
+                                                                                                                                                             
+  test('Rendering base e analisi HTML della vista checkout', () => {                                                                                         
+                                                                                                                                                             
+    const preloadedState = {                                                                                                                                 
+                                                                                                                                                             
+      autenticazione: {                                                                                                                                      
+                                                                                                                                                             
+        value: {                                                                                                                                             
+                                                                                                                                                             
+          id_utente: 1,                                                                                                                                      
+                                                                                                                                                             
+          isLogged: true,                                                                                                                                    
+                                                                                                                                                             
+          indirizzo: "Via Roma 10, Milano"                                                                                                                   
+                                                                                                                                                             
+        },                                                                                                                                                   
+                                                                                                                                                             
+      },                                                                                                                                                     
+                                                                                                                                                             
+      stile: {                                                                                                                                               
+                                                                                                                                                             
+        value: { vistaForm: 'form' },                                                                                                                        
+                                                                                                                                                             
+      },                                                                                                                                                     
+                                                                                                                                                             
+      carrello: {                                                                                                                                            
+                                                                                                                                                             
+        value: {                                                                                                                                             
+                                                                                                                                                             
+          items: [                                                                                                                                           
+                                                                                                                                                             
+            {                                                                                                                                                
+                                                                                                                                                             
+              id: 101,                                                                                                                                       
+                                                                                                                                                             
+              nome: "Servizio Test Checkout",                                                                                                                
+                                                                                                                                                             
+              prezzo: 25.00,                                                                                                                                 
+                                                                                                                                                             
+              quantita: 2,                                                                                                                                   
+                                                                                                                                                             
+              tipo: "Servizio"                                                                                                                               
+                                                                                                                                                             
+            }                                                                                                                                                
+                                                                                                                                                             
+          ]                                                                                                                                                  
+                                                                                                                                                             
+        },                                                                                                                                                   
+                                                                                                                                                             
+      },                                                                                                                                                     
+                                                                                                                                                             
+      carta: {                                                                                                                                               
+                                                                                                                                                             
+        value: { carte: [] },                                                                                                                                
+                                                                                                                                                             
+      },                                                                                                                                                     
+                                                                                                                                                             
+      servizio: {                                                                                                                                            
+                                                                                                                                                             
+        value: { servizi: [] },                                                                                                                              
+                                                                                                                                                             
+      },                                                                                                                                                     
+                                                                                                                                                             
+    };                                                                                                                                                       
+                                                                                                                                                             
+                                                                                                                                                             
+                                                                                                                                                             
+    renderWithProviders(<CheckoutView />, { preloadedState });                                                                                               
+                                                                                                                                                             
+                                                                                                                                                             
+                                                                                                                                                             
+    screen.debug();                                                                                                                                          
+                                                                                                                                                             
+  });                                                                                                                                                        
+                                                                                                                                                             
+});                              
